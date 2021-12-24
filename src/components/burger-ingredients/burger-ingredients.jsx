474197a3f3d8680a11180bb234e8624628b06232
oalbukova <o.albukova@gmail.com>
2021-12-24 20:45:35 +0300

    добавила onScroll

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -14,27 +14,65 @@ import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
 import { TAB_SWITCH } from "../../services/actions/cart";
 
 const BurgerIngredients = () => {
+  const burgerIngredientsElem = document.querySelector("#burger-ingredients");
+  const bunsElem = document.querySelector("#bun");
+  const saucesElem = document.querySelector("#sauce");
+  const toppingElem = document.querySelector("#topping");
+
   const dispatch = useDispatch();
   const { items, currentTab } = useSelector((state) => state.cart);
 
   const bunRef = useRef(null);
   const saucesRef = useRef(null);
-  const mainRef = useRef(null);
+  const toppingRef = useRef(null);
 
-  const setTab = (tab, tabRef) => {
+  const setCurrentTab = (tab) => {
     dispatch({
       type: TAB_SWITCH,
-      tab
+      tab,
     });
+  };
+  const setTab = (tab, tabRef) => {
+    setCurrentTab(tab);
     tabRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const onScrollPosition = () => {
+    const bunsPosition = () =>
+      Math.abs(
+        bunsElem.getBoundingClientRect().top -
+          burgerIngredientsElem.getBoundingClientRect().top
+      );
+
+    const saucesPosition = () =>
+      Math.abs(
+        saucesElem.getBoundingClientRect().top -
+          burgerIngredientsElem.getBoundingClientRect().top
+      );
+
+    const toppingPosition = () =>
+      Math.abs(
+        toppingElem.getBoundingClientRect().top -
+          burgerIngredientsElem.getBoundingClientRect().top
+      );
+
+    bunsPosition() < saucesPosition()
+      ? setCurrentTab("bun")
+      : saucesPosition() < toppingPosition()
+      ? setCurrentTab("sauce")
+      : setCurrentTab("topping");
   };
 
   const typeBun = items.filter((el) => el.type === "bun");
   const typeSauce = items.filter((el) => el.type === "sauce");
-  const typeMain = items.filter((el) => el.type === "main");
+  const typeTopping = items.filter((el) => el.type === "main");
 
   return (
-    <section className={styles.burgerIngredients}>
+    <section
+      className={styles.burgerIngredients}
+      id="burger-ingredients"
+      onScroll={() => onScrollPosition()}
+    >
       <div style={{ display: "flex" }}>
         <Tab
           value="bun"
@@ -44,16 +82,16 @@ const BurgerIngredients = () => {
           Булки
         </Tab>
         <Tab
-          value="sauces"
-          active={currentTab === "sauces"}
-          onClick={() => setTab("sauces", saucesRef)}
+          value="sauce"
+          active={currentTab === "sauce"}
+          onClick={() => setTab("sause", saucesRef)}
         >
           Соусы
         </Tab>
         <Tab
-          value="main"
-          active={currentTab === "main"}
-          onClick={() => setTab("main", mainRef)}
+          value="topping"
+          active={currentTab === "topping"}
+          onClick={() => setTab("topping", toppingRef)}
         >
           Начинки
         </Tab>
@@ -67,11 +105,7 @@ const BurgerIngredients = () => {
             <Ingredient key={item._id} ingredient={item} />
           ))}
         </ul>
-        <h2
-          className={`text text_type_main-medium`}
-          id="sauces"
-          ref={saucesRef}
-        >
+        <h2 className={`text text_type_main-medium`} id="sauce" ref={saucesRef}>
           Соусы
         </h2>
         <ul className={`${styles.list} mt-6 mb-10 ml-4 mr-2`}>
@@ -81,13 +115,13 @@ const BurgerIngredients = () => {
         </ul>
         <h2
           className={`text text_type_main-medium mb-3`}
-          id="main"
-          ref={mainRef}
+          id="topping"
+          ref={toppingRef}
         >
           Начинки
         </h2>
         <ul className={`${styles.list} mt-6 mb-10 ml-4 mr-2`}>
-          {typeMain.map((item) => (
+          {typeTopping.map((item) => (
             <Ingredient key={item._id} ingredient={item} />
           ))}
         </ul>
